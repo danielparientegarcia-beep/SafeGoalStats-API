eb74d7ed87a075590ec51ef71bed7fa0cff09eaa
danielparientegarcia-beep <danielparientegarcia@elpui.xeill.net>
2026-05-15 22:14:11 +0200

    cambio datebase

--- src/database.js
+++ src/database.js
@@ -1,13 +1,21 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool(process.env.MYSQL_URL);
+const pool = mysql.createPool({
+    host: process.env.MYSQLHOST,      // antes DB_HOST
+    user: process.env.MYSQLUSER,      // antes DB_USER
+    password: process.env.MYSQLPASSWORD, // antes DB_PASSWORD
+    database: process.env.MYSQL_DATABASE, // antes DB_NAME
+    port: process.env.MYSQLPORT || 3306,  // importante si Railway usa otro puerto
+    waitForConnections: true,
+    connectionLimit: 10,
+});
 
 // Verificar conexión al arrancar
 pool.getConnection()
-    .then(conn => {
+    .then(connection => {
         console.log('Conectado a MySQL correctamente');
-        conn.release();
+        connection.release();
     })
     .catch(err => {
         console.error('Error al conectar con MySQL:', err.message);
