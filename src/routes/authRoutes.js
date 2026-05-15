6366783edcecfe76c5b357f13967b94ebdc907fc
danielparientegarcia-beep <danielparientegarcia@elpui.xeill.net>
2026-05-16 01:02:20 +0200

    routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,15 +2,25 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+
 // LOGIN
 router.post('/login', authController.login);
+
 
 // REGISTRO
 router.post('/registro', authController.registro);
 
-// TEST para verificar que carga
+
+// TEST
 router.get('/test', (req, res) => {
     res.json({ ok: true, message: 'authRoutes funcionando' });
 });
+
+
+// DEBUG
+router.get('/debug', (req, res) => {
+    res.json({ ok: true, routes: 'authRoutes loaded' });
+});
+
 
 module.exports = router;
